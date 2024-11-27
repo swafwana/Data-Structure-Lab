26b03b829b822ad474b6e0c10cf8a93e4d405f01
swafwana <swafwanasheri17@gmail.com>
2024-11-27 16:29:00 +0530

    Add files via upload

--- doublelinked.c
+++ doublelinked.c
@@ -49,11 +49,49 @@ void insertAtEnd(int data)
 		newnode=create_node(data);
 		while(ptr->rlink!=NULL)
 		{
-			ptr=ptr->rlink
+			ptr=ptr->rlink;
 		}
+	
 		ptr->rlink=newnode;
 		newnode->llink=ptr;
 	}
+}
+void insertAtAny(int data,int position)
+{
+	struct node* newnode;
+	newnode=create_node(data);
+	struct node* temp = header;
+	for (int i = 0; i < position - 1 && temp != NULL; i++) 
+	{
+
+        	temp = temp->rlink;
+	}
+	
+	newnode->rlink = temp->rlink;
+	temp->rlink= newnode;
+
+	
+
+}
+void deleteAtFront()
+{
+	struct node* ptr;
+	ptr=header;
+	header=header->rlink;
+	free(ptr);
+}
+void deleteAtEnd()
+{
+	struct node* ptr;
+	struct node* ptr1;
+	ptr=header;
+	while(ptr->rlink!=NULL)
+	{
+		ptr1=ptr;
+		ptr=ptr->rlink;
+	}
+	ptr1->rlink=NULL;
+	free(ptr);
 }
 	
 void traversal()
@@ -62,7 +100,7 @@ void traversal()
 	ptr=header;
 	while(ptr!=NULL)
 	{
-		printf("%d",ptr->data);
+		printf("%d ",ptr->data);
 		ptr=ptr->rlink;
 	}
 }
@@ -71,7 +109,10 @@ int main (){
 	insertAtFront(5);
 	insertAtFront(4);
 	insertAtEnd(9);
+	insertAtAny(7,3);
 	insertAtEnd(6);
+	deleteAtFront();
+	deleteAtEnd();
 	traversal();
 	return(0);
 }
